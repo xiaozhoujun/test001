a97e6454d3ff8af863d747567b86437ad57df15e
xiaozhoujun <xiaozhou1650@gmail.com>
2020-04-13 15:11:24 +0800

    '重新提交'

---
+++ com/xzj/Test.java
@@ -0,0 +1,14 @@
+package com.xzj;
+
+/**
+ * @Author: xiaozhou
+ * @Date: 2020/4/13 15:02
+ * @Version: 1.0
+ * @Description:
+ */
+public class Test {
+    public static void main(String args[]){
+        System.out.println("Hello World");
+        System.out.println("Hello Github!");
+    }
+}
